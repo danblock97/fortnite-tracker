4f185b72fa24ca599a93f7ac996b54a4ec2f7961
danblock97 <generalgoldiez@gmail.com>
2024-04-22 18:05:01 +0100

    Add NavLink and MenuOverlay components to handle navigation in the app

---
+++ src/app/components/NavBar.js
@@ -0,0 +1,68 @@
+"use client";
+import Link from "next/link";
+import Image from "next/image";
+import React, { useState } from "react";
+import NavLink from "./NavLink";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
+import MenuOverlay from "./MenuOverlay.js";
+
+const navLinks = [
+	{
+		title: "Support",
+		path: "https://discord.gg/BeszQxTn9D",
+	},
+];
+
+const Navbar = () => {
+	const [navbarOpen, setNavbarOpen] = useState(false);
+
+	return (
+		<nav className="fixed mx-auto top-0 left-0 right-0 z-10 bg-[#381e80] bg-opacity-100">
+			<div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
+				<div className="flex items-center">
+					<Image
+						src="/images/logo.png"
+						alt="AstroStats Logo"
+						width={50}
+						height={50}
+					/>
+					<Link
+						href={"/"}
+						className="text-2xl md:text-3xl text-white font-semibold ml-2"
+					>
+						Forti Finder
+					</Link>
+				</div>
+				<div className="mobile-menu block md:hidden">
+					{!navbarOpen ? (
+						<button
+							onClick={() => setNavbarOpen(true)}
+							className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
+						>
+							<Bars3Icon className="h-5 w-5" />
+						</button>
+					) : (
+						<button
+							onClick={() => setNavbarOpen(false)}
+							className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
+						>
+							<XMarkIcon className="h-5 w-5" />
+						</button>
+					)}
+				</div>
+				<div className="menu hidden md:block md:w-auto" id="navbar">
+					<ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0">
+						{navLinks.map((link, index) => (
+							<li key={index}>
+								<NavLink href={link.path} title={link.title} />
+							</li>
+						))}
+					</ul>
+				</div>
+			</div>
+			{navbarOpen ? <MenuOverlay links={navLinks} /> : null}
+		</nav>
+	);
+};
+
+export default Navbar;
