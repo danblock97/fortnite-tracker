facf8661f1ad11d894b011f69238de31fa4f522c
danblock97 <generalgoldiez@gmail.com>
2024-06-05 13:29:01 +0100

    Added Loading.js to the application

---
+++ src/app/components/Loading.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Loading = () => {
+	return (
+		<div className="">
+			<div className="bg-[#23439b] flex items-center justify-center h-screen">
+				<div className="w-20 h-20 border-t-4 border-b-4 border-black rounded-full animate-spin"></div>
+			</div>
+		</div>
+	);
+};
+
+export default Loading;
--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -2,9 +2,14 @@
 
 import Profile from "../components/Profile";
 import useProfileData from "@/app/hooks/useProfileData";
+import Loading from "../components/Loading";
 
 const Page = () => {
-	const { profileData, error: profileError } = useProfileData();
+	const { profileData, isLoading, error: profileError } = useProfileData();
+
+	if (isLoading) {
+		return <Loading />;
+	}
 
 	return (
 		<div className="min-h-screen bg-[#23439b] overflow-hidden">
